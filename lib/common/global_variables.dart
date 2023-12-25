5967a0c55f319ee221cbc54befca40e41e903764
WARCRIT <akbarjakasyahputra@gmail.com>
2023-12-26 01:30:55 +0700

    meet 10

--- lib/common/global_variables.dart
+++ lib/common/global_variables.dart
@@ -1,3 +1,3 @@
 class GlobalVariables {
-  static const baseUrl = 'https://52ae-2404-8000-1028-1268-85ac-21ac-c4e-3a5e.ngrok-free.app';
+  static const baseUrl = 'https://39a0-2404-8000-1028-1268-f4ef-42e4-8fa8-189a.ngrok-free.app';
 }
